1d456346a0607215f64d0e10f82ecb239f8af802
Nikhil Srivastava <AppsrivTech4@nikhils-mac-mini.local>
2016-01-04 16:30:26 +0530

    Initial Commit

---
+++ Flocally/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flocally
+//
+//  Created by Nikhil Srivastava on 1/4/16.
+//  Copyright © 2016 Nikhil Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
